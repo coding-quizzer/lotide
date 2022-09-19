34b7deb55bc87b9d9f7ba5fcffe949a4774d5d64
William Gadd <wgadd@sasktel.net>
2022-09-19 22:18:37 +0000

    Used Template Literals in the place of concatination.

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("\u{1F7E2}Assertion Passed\u{1F7E2}: " + actual + " === " + expected);
+    console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
   } else {
-    console.log("\u{1F534}Assertion Failed\u{1F534}: " + actual + " !== " + expected);
+    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !==   ${expected}`);
   }
  
 };
