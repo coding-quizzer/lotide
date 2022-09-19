f925dc72cb9634e35c6347c5f81576d7902fe34d
William Gadd <wgadd@sasktel.net>
2022-09-19 22:02:22 +0000

    Created and implimented assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("\u{1F7E2}Assertion Passed\u{1F7E2}: " + actual + " === " + expected);
+  } else {
+    console.log("\u{1F534}Assertion Failed\u{1F534}: " + actual + " !== " + expected);
+  }
+ 
+};
+
+
+
+assertEqual('Lighthouse', 'Labs');
+assertEqual(1, 1);
+assertEqual(3, 30);
+assertEqual('Logo', "Logo");
