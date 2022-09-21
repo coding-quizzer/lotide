61388ff358d89cec9a17fb357aad59e61359f41d
William Gadd <wgadd@sasktel.net>
2022-09-21 17:36:15 +0000

    Changed console.log function in assertArraysEqual so that the arrays actually look like arrays

--- without.js
+++ without.js
@@ -11,9 +11,9 @@ const eqArrays = function(array1, array2) {
 const assertArraysEqual = function(actual, expected) {
 
   if (eqArrays(actual, expected)) {
-    console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
+    console.log("\u{1F7E2}Assertion Passed\u{1F7E2}:", actual, "===", expected);
   } else {
-    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !== ${expected}`);
+    console.log("\u{1F534}Assertion Failed\u{1F534}:", actual, "!==", expected);
   }
 
 };
