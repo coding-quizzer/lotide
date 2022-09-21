509b1a45d02f4c4754e4373f6ac6e47d9b18794f
William Gadd <wgadd@sasktel.net>
2022-09-21 21:06:10 +0000

    Implimented countOnly function

---
+++ countOnly.js
@@ -0,0 +1,59 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !== ${expected}`);
+  }
+ 
+};
+
+const countOnly = function (allItems, itemsToCount) {
+
+  const results  = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    
+    if(itemsToCount[item] === true) {
+
+      if(!results[item]) {
+        results[item] = 0;
+      }
+
+      results[item]++;
+    }
+
+  }
+
+  return results;
+
+};
+
+const firstNames = [
+
+  "Karl",
+ 
+  "Salima",
+
+  "Agouhanna",
+
+  "Fang",
+
+  "Kavith",
+
+  "Jason",
+
+  "Salima",
+
+  "Fang",
+
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {"Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
