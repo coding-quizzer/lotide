a6e64c569e340dd08b82671d8e2126bd377b5d07
William Gadd <wgadd@sasktel.net>
2022-09-22 20:49:02 +0000

    Added empty map function and an initial test

---
+++ map.js
@@ -0,0 +1,8 @@
+const map = function(array, mapFunction) {
+
+};
+
+const words = ["many", "more", "less", "spoon", "might"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
