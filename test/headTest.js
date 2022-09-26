9466cf147bd9b994d79a47f1e7c6c02d7250a9a6
William Gadd <wgadd@sasktel.net>
2022-09-26 17:38:21 +0000

    Moved assertEqual import from head.js to test/headTest.js, imported head function from head.js to test/headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,6 @@
+const assertEqual = require("../assertEqual");
+const head = require("../head");
+
 assertEqual(head([1, 3, 5]), 1);
 assertEqual(head(["fairy", "time", "stories"]), "fairy");
 assertEqual(head([4]), 4);
