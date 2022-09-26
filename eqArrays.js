4f5b6b04eb4883aa1f8b54f923f2c6bc9eeb7272
William Gadd <wgadd@sasktel.net>
2022-09-26 18:10:22 +0000

    Changed EqArrays function so that it uses assertEqual.js to access assertEqual function instead of a hardcoded function. Moved function tests to test/eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -4,7 +4,6 @@ const eqArrays = function(array1, array2) {
     return false;
   }
 
-
   // Returns True if there is at least one element thst is not equal in both arrays
   let arraysNotEqual = (array1.some((element, index) => {
     if (Array.isArray(element)) {
@@ -17,19 +16,4 @@ const eqArrays = function(array1, array2) {
 
 };
 
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !==   ${expected}`);
-  }
- 
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, [3, 4], 5, [6]], [1, 2, [3, 4, 5], [6]]), false);
-assertEqual(eqArrays([1, 2, [3, [4, 5]], [6]], [1, 2, [3, [4, 5]], [6]]), true);
+module.exports = eqArrays;
