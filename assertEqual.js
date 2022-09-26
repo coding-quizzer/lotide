04d95438fe1555e03721d41e457db09be14a53dc
William Gadd <wgadd@sasktel.net>
2022-09-26 17:40:43 +0000

    Removed excess spaces from Assertion Failed message

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
   } else {
-    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !==   ${expected}`);
+    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !== ${expected}`);
   }
  
 };
