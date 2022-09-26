87252a5bea2c27292b99b13b9c8729a2642f08aa
William Gadd <wgadd@sasktel.net>
2022-09-26 17:39:44 +0000

    Exported head function from head.js

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
-const assertEqual = require("./assertEqual");
-
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([1, 3, 5]), 1);
-assertEqual(head(["fairy", "time", "stories"]), "fairy");
-assertEqual(head([4]), 4);
-assertEqual(head([]), undefined);
+module.exports = head;
