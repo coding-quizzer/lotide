3f5942cce6d7dcb7e37791c27f029fa7bc9316d1
William Gadd <wgadd@sasktel.net>
2022-09-26 22:55:19 +0000

    Imported all the functions in the LoDash library into index.js"

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./eqArrays');
+const flatten = require('./flatten');
+const without = require('./without');
+const countLetters = require('./countLetters');
+const letterPositions = require('./letterPositions');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+
 
 module.exports = {
 
