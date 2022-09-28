8d4750d8d684f7cbde6b5621dcd2a2f47c41cfd6
William Gadd <wgadd@sasktel.net>
2022-09-28 14:27:23 +0000

    Imported Assert Objects Equal function to replace hardcoded pasted material. Moved tests to tes folder

--- countLetters.js
+++ countLetters.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`\u{1F7E2}Assertion Passed\u{1F7E2}: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F534}Assertion Failed\u{1F534}: ${actual} !==   ${expected}`);
-  }
- 
-};
+const assertEqual = require('./assertEqual');
 
 const countLetters = function(string) {
 
@@ -28,23 +20,5 @@ const countLetters = function(string) {
 
 };
 
-console.log(countLetters("This is a string"));
-let test1 = "Here is a good Test";
-let test2 = "The quick brown fox jumps over the lazy dogs";
-
-assertEqual(countLetters(test1).h, 1);
-assertEqual(countLetters(test1).e, 3);
-assertEqual(countLetters(test1).r, 1);
-assertEqual(countLetters(test1).i, 1);
-assertEqual(countLetters(test1).s, 2);
-assertEqual(countLetters(test1).a, 1);
-assertEqual(countLetters(test1).g, 1);
-assertEqual(countLetters(test1).o, 2);
-assertEqual(countLetters(test1).d, 1);
-assertEqual(countLetters(test1).t, 2);
-
-console.log("test:", test1);
-
-console.log(countLetters(test2));
 
 module.exports = countLetters;
